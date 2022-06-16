50dd2c8d4ccfb9550656ca7725d8b90017a3d160
BETH REICHERTER <bethreicherter@gmail.com>
2022-06-16 14:20:08 -0400

    let_const_exercise

---
+++ let_const_exercise.js
@@ -0,0 +1,15 @@
+const PI = 3.14
+PI = 42 // error
+
+// What is the difference between var and let?  
+// var can be redeclared and let can not. 
+
+
+// What is the different between var and const?
+// var can be reassigned and redeclared, const can not. 
+
+// What is the difference between let and const?
+// let can be reassigned, and const can not.
+
+// What is hoisting?
+// a process whereby the interpreter appears to move the declaration of functions, variable o or classes to the top of their scope, prior to the execution of the code.
